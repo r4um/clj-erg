94e74c9e02e71b0de5f6c381cb4cf06352c40117
Pranay Kanwar <pranay.kanwar@gmail.com>
2014-12-01 13:50:24 +0530

    Fix test add travis

--- test/erg/core_test.clj
+++ test/erg/core_test.clj
@@ -4,11 +4,11 @@
 
 (deftest erg-expand-test
   (testing "range expansion"
-    (def e #{"erg1" "erg2" "erg3"})
-    (def re (set (expand "erg1..3" :range-server "127.0.0.1")))
-    (is (= e re))))
+    (let [e #{"erg1" "erg2" "erg3"}
+          re (set (expand "erg1..3" :range-server "127.0.0.1")) ]
+      (is (= e re)))))
 
 (deftest erg-empty-test
   (testing "non existent expansion should return empty vector"
-    (def re (expand "%does_not_exist" :range-server "127.0.0.1"))
-    (is (empty? re))))
+    (let [re (expand "%does_not_exist" :range-server "127.0.0.1")]
+      (is (empty? re)))))
